5430ffe4876327e56298fbe9a340d76ec470ecff
filipenevola <filipenevola@gmail.com>
2020-04-28 09:08:26 -0400

    v0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'quave:collections',
-  version: '0.0.4-beta.2',
+  version: '0.0.4',
   summary: 'Utility package to create Meteor collections in a standard way',
   git: 'https://github.com/quavedev/collections',
 });
